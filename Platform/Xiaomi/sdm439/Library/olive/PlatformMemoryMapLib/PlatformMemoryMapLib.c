8f2a5858b46b58a062a509957bf6a68524e19a65
jhonpetter <jhon_petter_1988@hotmail.com>
2026-05-13 11:10:41 -0300

    Update PlatformMemoryMapLib.c

--- Platform/Xiaomi/sdm439/Library/olive/PlatformMemoryMapLib/PlatformMemoryMapLib.c
+++ Platform/Xiaomi/sdm439/Library/olive/PlatformMemoryMapLib/PlatformMemoryMapLib.c
@@ -35,14 +35,13 @@ static ARM_MEMORY_REGION_DESCRIPTOR_EX gDeviceMemoryDescriptorEx[] = {
     {"FV Region",         0x9F800000, 0x00400000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},
     {"DBI Dump",          0x9FC00000, 0x00300000, NoHob,  MMAP_IO, INITIALIZED, Reserv, UNCACHED_UNBUFFERED_XN},
     
-    /* Região PStore (Extraído do DTS) */
+    /* Região PStore (Ajustado para NoHob conforme DTS) */
     {"PStore Region",     0x9FF00000, 0x00100000, NoHob,  MEM_RES, SYS_MEM_CAP, Reserv, NS_DEVICE},
     
-    /* Estruturas Críticas do Processador */
+    /* Estruturas Críticas do Processador (Alinhamento de 64KB para Windows) */
     {"CPU Vectors",       0x9FFC0000, 0x00001000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
     {"MMU PageTables",    0x9FFD0000, 0x00010000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},
-    {"UEFI Stack",        0x9FFE0000, 0x00020000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},
-    // 
+    {"UEFI Stack",        0x9FFE0000, 0x00020000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},    // 
     {"RSRV1",             0x9FFD0000, 0x0000A000, AddMem, SYS_MEM, SYS_MEM_CAP, Reserv, WRITE_BACK_XN},
     {"TPMControl",        0x9FFDA000, 0x00003000, AddMem, MEM_RES, WRITE_COMBINEABLE, Reserv, UNCACHED_UNBUFFERED_XN},
     {"Reset Data",        0x9FFDD000, 0x00004000, AddMem, SYS_MEM, SYS_MEM_CAP, Reserv, UNCACHED_UNBUFFERED_XN},
